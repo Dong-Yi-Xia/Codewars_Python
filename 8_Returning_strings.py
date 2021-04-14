bcc969bd5bf6d38f93dd9a764040e0d6e74003c3
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-04-14 09:58:05 -0400

    return string

---
+++ 8_Returning_strings.py
@@ -0,0 +1,8 @@
+# Returning Strings
+
+# Make a function that will return a greeting statement 
+# that uses an input; your program should return, "Hello, how are you doing today?".
+
+
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
