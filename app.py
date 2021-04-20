3548c37abceb1922c059f5b4b64d00481c455d93
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-04-20 15:38:32 -0400

    python basis

---
+++ app.py
@@ -0,0 +1,136 @@
+# TO RUN THE FILE --> python app.py or python3 app.py 
+# similar to Ruby --> ruby app.rb
+
+
+# variables
+# first_name = "John"
+# last_name = "Doe"
+
+
+# using input()
+# name = input('What is your name: ')
+# print('My name is ' + name)
+
+
+# conversion
+# int()
+# float()
+# str()
+# first = "10"
+# second = 3
+# sum = int(first) + second
+# print('the sum is ' + str(sum))
+
+
+# strings methods
+# .upper()
+# .find()
+# .replace()
+
+
+# add +
+# minus -
+# multiply *
+# divide /
+# whole num divide //
+# mod %
+
+
+# logicals 
+# >
+# <
+# ==
+# !=
+# and
+# or
+# True
+# False
+# not
+
+
+# if statements
+# if num > 10:
+#     print(greater than 10)
+# elif num == 10:
+#     print(number is 10)
+# else:
+#     print(less than 10)
+
+
+# weight = int( input('weight: ') )
+# unit = input('K(g) or (L)bs' )
+
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print('weight in Lbs: ' + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print('weight in kgs: ' + str(converted))
+
+
+# while loops
+# can write 1000 as 1000 or 1_000
+# i = 1
+# while i <= 10:
+#     print(i * '$')
+#     i += 1
+
+
+# list or array
+# names = ['John', 'Bob', 'Sam', 'Chris', "Mary"]
+# names[0] = "Joe"
+# print(names[0])
+# print(names[-1])
+# print(names[0:3]) #just like names.slice(0,3)
+
+# numbers = [1,2,3,4,5]
+# numbers.append(6)
+# numbers.insert(1, "A")
+# numbers.remove(4)
+# numbers.clear() #give back an empty []
+# print(numbers)
+# print(1 in numbers) #return True or False
+# print(len(numbers))  #like JS .length()
+
+
+# For Loops
+# numbers = [1,2,3,4,5]
+# for item in numbers: 
+#     print(item)
+
+# same as JS
+# let numbers = [1,2,3,4,5]
+# for (item of numbers){
+#   console.log(item)
+# }   
+
+
+# Range Function
+# ending range isn't included
+# num = range(5, 10, 2)
+# for n in num:
+#     print(n) 
+
+
+
+# Tuples are immutable, are like list but can't change  
+# numbers = (1,2,3,4,2,2)
+# print (numbers.count(2))
+# print( numbers.index(3))
+
+
+# dictionary
+# dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+# print(dict["Name"])
+
+
+
+# def currentAge(age):
+#     return age
+
+
+
+
+# import math
+# x = math.sqrt(64)
+# print(x)
